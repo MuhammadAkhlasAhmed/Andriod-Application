ac91e93d663818d12b3042124eedeb2e7bdf51f3
aqhayat <aqib.hayat@yahoo.com>
2017-12-01 21:08:50 +0500

    some code alignment in firebase service.

--- FIREBASE-Service/src/main/java/com/app/Application.java
+++ FIREBASE-Service/src/main/java/com/app/Application.java
@@ -5,12 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	SpringApplication.run(Application.class,args);
 	}
-
 }
--- FIREBASE-Service/src/main/java/com/app/service/impl/AppServiceImpl.java
+++ FIREBASE-Service/src/main/java/com/app/service/impl/AppServiceImpl.java
@@ -14,7 +14,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Map.Entry;
 
@@ -36,103 +35,65 @@ public class AppServiceImpl implements IAppService {
 			conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestProperty("Accept", "application/json");
 			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-			System.out.println("Output from Server .... \n");
-
 			while ((output = br.readLine()) != null) {
-
 				JSONObject jsonObj = new JSONObject(output);
 				JSONArray[] jsonArray = new JSONArray[jsonObj.length()];
-
 				for (int i = 0; i < jsonObj.length(); i++) {
 					jsonArray[i] = jsonObj.getJSONArray(jsonObj.names().optString(i));
 				}
-
 				for (int k = 0; k < jsonObj.length(); k++) {
 					for (int l = 0; l < jsonArray[k].length(); l++) {
 						list.add(jsonArray[k].getString(l));
 					}
 				}
-
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Exception "+e);
 		} finally {
 			conn.disconnect();
-
 		}
-
 		return list;
 	}
 
 	public int getMoviesCount(String Name) {
 		List<String> movieList = getFireBaseMovies();
 		Hashtable<String, Integer> Hset = new Hashtable<String, Integer>();
-		// Hashtable<String, Integer> Fset = new Hashtable<String, Integer>();
 		Map<String, Integer> Fset = new HashMap<String, Integer>();
 		Set<String> unique = new HashSet<String>(movieList);
 		for (String key : unique) {
-			// System.out.println(key + ": " + Collections.frequency(movieList, key));
 			Hset.put(key.toLowerCase(), Collections.frequency(movieList, key));
-
 		}
-
 		String a = Name.toLowerCase();
-
-		// String str1 = a;
 		String str = a;
 		String[] j = str.split("\\s+");
-
 		int count = 0;
 		for (String s : j) {
 			for (String key : Hset.keySet()) {
-
 				if (Pattern.compile(s).matcher(key).find()) {
-
 					Fset.put(key, count++);
-
 				}
-
 			}
-
 		}
-
 		Set<Entry<String, Integer>> set = Fset.entrySet();
 		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
-
 		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
 			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
 				return (o2.getValue()).compareTo(o1.getValue());
 			}
 		});
-
 		Map<String, Integer> aMap2 = new LinkedHashMap<String, Integer>();
 		for (Entry<String, Integer> entry : list) {
 			aMap2.put(entry.getKey(), entry.getValue());
 		}
-
-		// for(Entry<String,Integer> entry : aMap2.entrySet()) {
-		// System.out.println(entry.getKey() + " - " + entry.getValue());
-		//
-		// }
-
 		final Set<Entry<String, Integer>> mapValues = aMap2.entrySet();
 		final int maplength = mapValues.size();
 		final Entry<Integer, String>[] test = new Entry[maplength];
 		mapValues.toArray(test);
-
 		System.out.println("Here is your size of patern matching" + test[0].getKey());
-
-		// System.out.println("Here is your size of patern matching"+rrrrrr.length());
-
 		if (aMap2.size() != 0) {
 			return Hset.get(test[0].getKey());
 		} else {
-
 			return 1;
-
 		}
-
 	}
-
 }
