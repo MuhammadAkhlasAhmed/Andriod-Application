e74a0abc0433745663e5dd7d2c08fd50c3273ee7
aqhayat <aqib.hayat@yahoo.com>
2017-12-01 06:20:17 +0500

    modification in imdb-service

--- IMDB-Service/src/main/java/com/application/service/impl/AppServiceImpl.java
+++ IMDB-Service/src/main/java/com/application/service/impl/AppServiceImpl.java
@@ -7,7 +7,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
