eebf2987b7ba571a3f510a4e3740ecd8a42e4c07
ahayat-nisum-com <ahayat@nisum.com>
2017-12-09 02:23:12 +0500

    remove unsused imports.

--- IMDB-WebSerivce/src/main/java/com/app/util/MovieUtils.java
+++ IMDB-WebSerivce/src/main/java/com/app/util/MovieUtils.java
@@ -2,19 +2,14 @@ package com.app.util;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 import com.app.dto.MovieDTO;
 import com.app.dto.MovieFilterDTO;
