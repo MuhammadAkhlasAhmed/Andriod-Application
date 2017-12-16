55cb9671132493715bfcb6515d9471a9590106d0
MuhammadAkhlasAhmed <m.akhlasahmed@hotmail.com>
2017-12-17 03:48:00 +0500

    ALL CODE REVISED
    Due to Some Problems and name conflicts i update whole project files names and classes and layouts names also added the new pictures and data

--- Andriod Application/app/src/main/java/com/example/muhammadikhlas/myapplication/FirebaseNotificationResultActivity.java
+++ Andriod Application/app/src/main/java/com/example/muhammadikhlas/myapplication/FirebaseNotificationResultActivity.java
@@ -1,17 +1,15 @@
 package com.example.muhammadikhlas.myapplication;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class IkhlasServiceWali extends Activity {
+public class FirebaseNotificationResultActivity extends Activity {
 public TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ikhlas_service_wali);
+        setContentView(R.layout.firebasenotificationresultactivit);
        txt=(TextView)findViewById(R.id.mytext);
 
         String value=getIntent().getExtras().getString("Moviename");
