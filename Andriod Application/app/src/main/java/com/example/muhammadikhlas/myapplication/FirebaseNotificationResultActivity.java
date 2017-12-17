367d73cc0ee93292f1fa7561ea4882a129e657ad
MuhammadAkhlasAhmed <m.akhlasahmed@hotmail.com>
2017-12-18 02:25:18 +0500

    Changes
    Changes

--- Andriod Application/app/src/main/java/com/example/muhammadikhlas/myapplication/FirebaseNotificationResultActivity.java
+++ Andriod Application/app/src/main/java/com/example/muhammadikhlas/myapplication/FirebaseNotificationResultActivity.java
@@ -1,11 +1,15 @@
 package com.example.muhammadikhlas.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class FirebaseNotificationResultActivity extends Activity {
+public class FirebaseNotificationResultActivity extends Activity implements View.OnClickListener{
 public TextView txt;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,24 @@ public TextView txt;
         String value=getIntent().getExtras().getString("Moviename");
         txt.setText(""+value);
 
+b1=(Button)findViewById(R.id.button18);
+        b1.setOnClickListener(this);
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==b1){
+            Intent in=new Intent(FirebaseNotificationResultActivity.this,LogoutActivity.class);
+            startActivity(in);
+
+        }
+
+        //if(view==b2){}
     }
 }
