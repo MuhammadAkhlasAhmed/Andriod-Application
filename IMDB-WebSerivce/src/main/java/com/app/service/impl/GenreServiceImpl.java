7a0b79f02344f80263f37991b20573ff871f9e21
ahayat-nisum-com <ahayat@nisum.com>
2017-12-24 04:37:00 +0500

    web matrix algo completed.

--- IMDB-WebSerivce/src/main/java/com/app/service/impl/GenreServiceImpl.java
+++ IMDB-WebSerivce/src/main/java/com/app/service/impl/GenreServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.app.dto.FinalMovieDto;
