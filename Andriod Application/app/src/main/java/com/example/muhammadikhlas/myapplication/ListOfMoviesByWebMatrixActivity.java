b74f39d62775ca84366876d89d8cb6cd95dc643b
MuhammadAkhlasAhmed <m.akhlasahmed@hotmail.com>
2017-12-24 06:06:56 +0500

    Final Andriod Update With Custom Adapter
    The Only thing is left to connect this custom listview with aqib service final result after that there are 3 more work to be done fire base service connection , realtime database, exception handling.!

--- Andriod Application/app/src/main/java/com/example/muhammadikhlas/myapplication/ListOfMoviesByWebMatrixActivity.java
+++ Andriod Application/app/src/main/java/com/example/muhammadikhlas/myapplication/ListOfMoviesByWebMatrixActivity.java
@@ -3,12 +3,15 @@ package com.example.muhammadikhlas.myapplication;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListOfMoviesByWebMatrixActivity extends Activity {
 
@@ -21,27 +24,31 @@ public class ListOfMoviesByWebMatrixActivity extends Activity {
     StringBuffer responce;
 
 
+    String[] member_names= new String[5];
+    String[] statues=new String[5];
+    String[] contactType=new String[5];
+    List<RowItem> rowItems;
+    ListView mylistview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listofmoviesbywebmatrixactivity);
         activity = this;
-String name[]=new String[]{"Ikhlas","Ahmed","Nihal","Ahsan"};
-        ArrayAdapter<String> adpt=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
-                name);
+member_names[0]="Hello";
+statues[0]="1";
+        contactType[0]="111";
+
+        rowItems = new ArrayList<RowItem>();
 
 
-        listView = (ListView) findViewById(android.R.id.list);
-                //Call WebService
-listView.setAdapter(adpt);
+            RowItem item = new RowItem(member_names[0],statues[0],contactType[0]);
+            rowItems.add(item);
 
-
-
-        String path = "http://192.168.0.106:8080/api/v1/imdb/Inception";
-
-        URL url;
-        String Responcetext;
-
+        mylistview = (ListView) findViewById(R.id.list);
+        mylistview.setBackgroundColor(Color.BLACK);
+        CustomAdapter adapter = new CustomAdapter(this, rowItems);
+        mylistview.setAdapter(adapter);
 
 
 
---
+++ Andriod Application/app/src/main/java/com/example/muhammadikhlas/myapplication/MoviePojo.java
@@ -0,0 +1,20 @@
+package com.example.muhammadikhlas.myapplication;
+
+/**
+ * Created by Muhammad IKHLAS on 12/24/2017.
+ */
+
+public class MoviePojo {
+    int rating;
+    String tagline;
+    String name;
+
+    public  MoviePojo(int rating,String tagline,String name){
+
+        this.rating=rating;
+        this.tagline=tagline;
+        this.name=name;
+    }
+
+
+}
