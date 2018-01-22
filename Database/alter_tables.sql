1458931191884bbfabd32b5c311ddd21f67ec0cc
ahayat-nisum-com <ahayat@nisum.com>
2018-01-22 18:15:04 +0500

    db changes.

--- Database/alter_tables.sql
+++ Database/alter_tables.sql
@@ -1,8 +1,9 @@
 ALTER TABLE `item_review` 
 CHANGE COLUMN `item_id` `item_id` INT(11) NOT NULL AUTO_INCREMENT ;
 
-ALTER TABLE `item_review` 
-CHANGE COLUMN `item_id` `item_id` INT(11) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE `movies_title`
+
+CHANGE COLUMN `movie_id` `movie_id` INT(11) NOT NULL AUTO_INCREMENT ;
 
 ALTER TABLE `item_review` 
 DROP COLUMN `foreign`;
