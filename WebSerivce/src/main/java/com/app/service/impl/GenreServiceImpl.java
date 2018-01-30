a8747b3f0ccb41b60098c5402bf93fda7fb822e6
ahayat-nisum-com <ahayat@nisum.com>
2018-01-31 01:24:34 +0500

    code refactoring mention method comments

--- WebSerivce/src/main/java/com/app/service/impl/GenreServiceImpl.java
+++ WebSerivce/src/main/java/com/app/service/impl/GenreServiceImpl.java
@@ -30,33 +30,56 @@ public class GenreServiceImpl implements GenreService {
 	
 	@Override
 	public List<FinalMovieDto> getMovieGenreFromIMDB(String name) {
+		//get genres from Api
 		GenreDTO genreDTO = GenreUtils.getMovieGenre(name);
 		if (genreDTO.getGenres() != null) {
+			// get list of that genres
 			List<String> listOfGenre = genreDTO.getGenres();
 	    	List<String> listOfGenresReplaceBiographyToDocumentary = GenreUtils.replaceGenreBiographyToDocumentary(listOfGenre);
 	    	List<String> listOfGenresReplaceAdultToRomance =  GenreUtils.replaceGenreAdultToRomance(listOfGenresReplaceBiographyToDocumentary);
 	    	List<String> listOfGenresReplaceShortToHorror = GenreUtils.replaceGenreShortToHorror(listOfGenresReplaceAdultToRomance);
 	    	List<String> listOfGenresReplaceTalkShowToSciFi = GenreUtils.replaceGenreTalkShowToSciFi(listOfGenresReplaceShortToHorror);
-			String genre = GenreUtils.getGenre(listOfGenresReplaceTalkShowToSciFi);
-			String originalMovieTile = GenreUtils.getOriginalMovieTitle(name);
+	    	String genre = GenreUtils.getGenre(listOfGenresReplaceTalkShowToSciFi);
+
+	    	// get movie title from api
+	    	String originalMovieTile = GenreUtils.getOriginalMovieTitle(name);
+	    	
+	    	// get user given movie Object from DB against name
 			MovieDTO userGivenMovie = movieService.getMovieByName(originalMovieTile);
+			
+			// get movie Object from DB against genre 
 			MovieDTO movieDTO = movieService.getMovieByGenre(genre);
+			
+			// get movie attributes
 			int runtime = movieDTO.getRuntime();
 			BigInteger revenue = movieDTO.getRevenue();
 			double voteAverage = movieDTO.getVoteAverage();
 			int voteCount = movieDTO.getVoteCount();
 			double popularity = movieDTO.getPopularity();
 			BigInteger budget = movieDTO.getBudget();
-			List<Movie> listOFMovies = movieService.findMovie(runtime, revenue, voteAverage, voteCount, popularity, budget);
-			List<Movie> findAllMovie = movieService.findAllMovie(runtime, revenue, voteAverage, voteCount, popularity, budget);
-			List<String> moviesWithDistance =	MovieUtils.ContentBasedFiltering(MovieUtils.getListOfMovieDTO(listOFMovies), userGivenMovie);
+			
+			// get list of top twenty movies object against attributes for content based filtering
+			List<Movie> listOfTwentyMovies = movieService.findMovie(runtime, revenue, voteAverage, voteCount, popularity, budget);
+			
+			// get list of three hundred movies object against attributes for web matrix factorization
+			List<Movie> listOfThreeHundredMovies = movieService.findAllMovie(runtime, revenue, voteAverage, voteCount, popularity, budget);
+			
+			// get list of all movies name whose distance is nearest to user given movie
+			List<String> moviesWithDistance =	MovieUtils.ContentBasedFiltering(MovieUtils.getListOfMovieDTO(listOfTwentyMovies), userGivenMovie);
+			
+			// get list of five movies name whose distance is nearest to user given movie
 			List<String> listOfFirstFiveMovieName =	MovieUtils.getFirstFiveMovieName(moviesWithDistance);
+			
 			List<Movie>  listOfFirstFiveMovieObject = new ArrayList<Movie>();
+			
+			// iterate list that contain movie name and save its object in another list
 			for (String eachMovie : listOfFirstFiveMovieName) {
 				Movie movie = movieService.getMovieByItsName(eachMovie);
 				listOfFirstFiveMovieObject.add(movie);
 				}
-			List<FinalMovieDto> listOfFinalMovieDTO = AlgoUtils.WebMatrixFactorizationTechnique(MovieUtils.getListOfMovieRecordDTO(findAllMovie), listOfFirstFiveMovieObject);
+			
+			// get list of movies to predict perform web matrix factorization
+			List<FinalMovieDto> listOfFinalMovieDTO = AlgoUtils.WebMatrixFactorizationTechnique(MovieUtils.getListOfMovieRecordDTO(listOfThreeHundredMovies), listOfFirstFiveMovieObject);
 			return listOfFinalMovieDTO;
 		} else {
 			List<FinalMovieDto> list = new ArrayList<FinalMovieDto>();
